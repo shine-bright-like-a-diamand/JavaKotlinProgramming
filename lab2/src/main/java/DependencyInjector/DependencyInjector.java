f101f847981a5a67a927f660599e258f688c9c4c
polina <dobryninapolina500@gmail.com>
2021-10-12 22:30:31 +0300

    magic

--- lab2/src/main/java/DependencyInjector/DependencyInjector.java
+++
@@ -1,8 +0,0 @@
-package DependencyInjector;
-
-public interface DependencyInjector {
-    void register(Class<?> cl);
-    void register(Class<?> interf, Class<?> cl);
-    void completeRegistration();
-    <T> T resolve(Class<T> cl);
-}
--- lab2/src/main/java/Tests/AlsoInterface.java
+++
@@ -1,7 +0,0 @@
-package Tests;
-
-public interface AlsoInterface extends Interface {
-
-    void setString(String string);
-
-}
